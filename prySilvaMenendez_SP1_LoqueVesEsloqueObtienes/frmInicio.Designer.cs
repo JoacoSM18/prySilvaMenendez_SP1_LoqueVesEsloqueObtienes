5cc01800053217e098773ce148b00e905aa97675
JoacoSM18 <joako.sm.18@gmail.com>
2025-11-04 22:16:42 -0300

    Listo

--- prySilvaMenendez_SP1_LoqueVesEsloqueObtienes/frmInicio.Designer.cs
+++ prySilvaMenendez_SP1_LoqueVesEsloqueObtienes/frmInicio.Designer.cs
@@ -35,12 +35,11 @@
             // 
             // lblBienvenida
             // 
-            this.lblBienvenida.AutoSize = true;
             this.lblBienvenida.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblBienvenida.ForeColor = System.Drawing.Color.SteelBlue;
-            this.lblBienvenida.Location = new System.Drawing.Point(12, 57);
+            this.lblBienvenida.Location = new System.Drawing.Point(12, 38);
             this.lblBienvenida.Name = "lblBienvenida";
-            this.lblBienvenida.Size = new System.Drawing.Size(595, 39);
+            this.lblBienvenida.Size = new System.Drawing.Size(659, 39);
             this.lblBienvenida.TabIndex = 0;
             this.lblBienvenida.Text = "Bienvenido Al Sistema de ONE S.A.";
             this.lblBienvenida.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -48,7 +47,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::prySilvaMenendez_SP1_LoqueVesEsloqueObtienes.Properties.Resources.images__2_;
-            this.pictureBox1.Location = new System.Drawing.Point(145, 149);
+            this.pictureBox1.Location = new System.Drawing.Point(184, 149);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(334, 221);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -60,16 +59,16 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkKhaki;
-            this.ClientSize = new System.Drawing.Size(619, 427);
+            this.ClientSize = new System.Drawing.Size(683, 467);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lblBienvenida);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "frmInicio";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ingreso de Datos ONE S.A.";
             this.Load += new System.EventHandler(this.frmInicio_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
